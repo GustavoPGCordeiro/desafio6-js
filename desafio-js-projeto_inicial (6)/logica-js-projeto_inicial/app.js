ea6c81ba06701cc4ff294ead7edc5ad48ca7cc68
GustavoPGCordeiro <gustavopgcordeiro@gmail.com>
2025-02-07 16:56:37 -0300

    Add files via upload

---
+++ desafio-js-projeto_inicial (6)/logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+//Exercício 1 - Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+
+//Exercício 2 - Crie uma lista de linguagens de programação chamada linguagensDeProgramacao 
+// com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//Exercício 3 - Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+
+//Exercício 4 - Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaDeCores = ['Roxo','Azul','Vermelho'];
+console.log(listaDeCores[0]);
+
+
+//Exercício 5 - Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaDeMusicas = ['This I love' ,'King For A Day','Do I Wanna Know?'];
+console.log(listaDeMusicas[1]);
+
+//Exercício 6 -Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let listaDeNomes = ['Julia','Laura','Isadora'];
+console.log(listaDeNomes[listaDeNomes.length - 1]);
